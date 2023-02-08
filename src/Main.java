fb4d4e5bc0f0d5e9db1d0c3d2103689282830e7d
ya121 <ya7771@yandex.ru>
2023-02-08 16:12:13 +0300

    Сделано ДЗ № 2_3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import transport.Bus;
 import transport.Car;
 
 public class Main {
@@ -7,24 +8,31 @@ public class Main {
         System.out.println("Задача 2_1");
         System.out.println("Добавлены новые свойства в класс Car.");
         Car.Key ladaKey = new Car.Key(false, false);
-        Car ladaGranta = new Car("Lada", "Lada Granta", 1.7, "желтый", 2015, "Россия", "МКПП", "седан", "а001аа999",5,Car.summer, ladaKey);
+        Car ladaGranta = new Car("Lada", "Lada Granta", 2015, "Россия", "желтый", 150, 1.7, "МКПП", "седан", "а001аа999", 5, Car.summer, ladaKey);
         System.out.println(ladaGranta);
         Car.Key audiKey = new Car.Key(true, true);
-        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия", "АКПП", "седан", "а002аа999", 5, !Car.summer, audiKey);
+        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "черный", 300, 3.0, "АКПП", "седан", "а002аа999", 5,Car.summer, audiKey);
         System.out.println(audiA8);
         Car.Key bmwKey = new Car.Key(true, true);
-        Car bmwZ8 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия", "АКПП", "родстер", "а003аа999", 2, false, bmwKey);
+        Car bmwZ8 = new Car("BMW", "Z8", 2021, "Германия", "черный", 250, 3.0, "АКПП", "родстер", "а003аа999", 2, Car.summer, bmwKey);
         System.out.println(bmwZ8);
         Car.Key kiaKey = new Car.Key(true, false);
-        Car kiaSportage = new Car("KIA", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея", "АКПП", "внедорожник", "а004аа999", 5, false, kiaKey);
+        Car kiaSportage = new Car("KIA", "Sportage 4-го поколения", 2018, "Южная Корея", "черный", 180, 2.4, "АКПП", "внедорожник", "а004аа999", 5, Car.summer, kiaKey);
         System.out.println(kiaSportage);
         Car.Key hyundayKey = new Car.Key(false, false);
-        Car hyundayAvante = new Car("Hyunday", "Avante", 1.6, "оранжевый", 2016, "Южная Корея", "МКПП", "седан", "а005аа999", 5, true, hyundayKey);
+        Car hyundayAvante = new Car("Hyunday", "Avante", 2016, "Южная Корея", "черный", 180, 1.6, "МКПП", "седан", "а005аа999", 5, !Car.summer, hyundayKey);
         System.out.println(hyundayAvante);
         System.out.println("Смена шин на сезонные");
         ladaGranta.changeTireToSeason(2);
         System.out.println(ladaGranta);
         System.out.println();
+
+        Bus bus_1 = new Bus("Лиаз", "Лиаз1", 2022, "Россия", "зеленый", 150);
+        System.out.println(bus_1);
+        Bus bus_2 = new Bus("Лиаз", "Лиаз1", 2021, "Россия", "белый", 150);
+        System.out.println(bus_2);
+        Bus bus_3 = new Bus("Лиаз", "Лиаз1",2020, "Россия","",150);
+        System.out.println(bus_3);
 
     }
 
